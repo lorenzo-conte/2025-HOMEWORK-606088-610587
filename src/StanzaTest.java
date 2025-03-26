78556495f9a879c43ffa1086696b220e7c90d627
lorenzo-conte <lor.conte2@stud.uniroma3.it>
2025-03-26 16:20:48 +0100

    ulteriori modifiche
    suddivisione dei 2 test

--- src/StanzaTest.java
+++ src/StanzaTest.java
@@ -4,12 +4,25 @@ import org.junit.jupiter.api.Test;
 
 class StanzaTest {
 	
+	
+	
+	
 	@Test
-	void PrimaStanzaNord () {
+	void AtrioEguale () {
 	    Partita partita = new Partita();
 		Stanza atrio= partita.getStanzaCorrente();
 		
 		assertEquals("Atrio",atrio.getNome());
+		
+		
+		
+	}
+	@Test
+	void TestDirezioniAtrio () {
+	    Partita partita = new Partita();
+		Stanza atrio= partita.getStanzaCorrente();
+		
+		
 		assertNotNull(atrio.getDirezioni(), "L'array dovrebbe avere un array di direzioni");
 		
 		
